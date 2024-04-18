672a4ba64875aa7160200196f397671695fb6959
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2024-04-18 20:04:51 +0100

    Add config for hosting on github pages

---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+    base: 'https://alexmakessoftware.github.io/Sidewinder/' // Replace 'your-project-name' with the actual name of your project or the subdirectory where your project is hosted
+}
